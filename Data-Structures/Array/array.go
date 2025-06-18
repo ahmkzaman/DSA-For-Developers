04cedcd353cebab50495f81f2a941ebaf7bf1624
ahmkzaman <kzaman.ahm@gmail.com>
2025-06-18 15:12:27 +0600

    Golang array added

---
+++ Data-Structures/Array/array.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var arr [5]int // Declare an array of 5 integers
+	arr[0] = 10   // Assign value to the first element
+	arr[1] = 20
+	arr[2] = 30   // Assign value to the second element
+	arr[3] = 40   // Assign value to the third element
+	arr[4] = 50   // Assign value to the fourth element
+	fmt.Println("Array elements:", arr) // Print the array elements
+}

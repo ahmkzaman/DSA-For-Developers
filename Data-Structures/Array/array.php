91d7911a1c21a06c8c23c2ee62937f3b3b03e351
ahmkzaman <kzaman.ahm@gmail.com>
2025-06-18 13:33:22 +0600

    php array added

---
+++ Data-Structures/Array/array.php
@@ -0,0 +1,15 @@
+<?php
+// This file demonstrates the creation of arrays in PHP
+$arr1 = array(1, 2, 3, 4, 5);
+$arr2 = [6, 7, 8, 9, 10];
+// Associative array
+$arr3 = [
+    "name" => "John",
+    "age" => 30,
+    "city" => "New York"
+];
+// Multidimensional array
+$arr4 = [
+    "fruits" => ["apple", "banana", "cherry"],
+    "vegetables" => ["carrot", "broccoli", "spinach"]
+];
